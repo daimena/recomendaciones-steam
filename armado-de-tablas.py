9ddf11b3706bcd27176f42fe8ab296ce40736a01
daimena <daiana.mena@gmail.com>
2023-10-03 12:32:17 -0300

    Agregado batching y argparse para el armado de tablas

--- armado-de-tablas.py
+++ armado-de-tablas.py
@@ -2,11 +2,22 @@ import psycopg2
 from psycopg2 import extras
 import csv
 from tqdm import tqdm
+import argparse
+
+parser = argparse.ArgumentParser(description='Creación y poblado de tablas')
+parser.add_argument('--dbname', help='nombre de la base de datos', default='recomendaciones_steam')
+parser.add_argument('--user', help='nombre de usuario en la base de datos', default='postgres')
+parser.add_argument('--password', help='contraseña en la base de datos', default='postgres')
+parser.add_argument('--host', help='dirección del host', default='localhost')
+parser.add_argument('--port', help='puerto del host', default=5432)
+args = parser.parse_args()
 
 # Vamos a armar una base de datos en postgresql y cargar los datos obtenidos de los CSV preprocesados.
 
 #Establecer la conexion
-with psycopg2.connect(dbname="recomendaciones_steam", user="postgres", password="postgres") as conn:
+with psycopg2.connect(dbname=args.dbname, user=args.user, password=args.password, host=args.host, port=args.port) as conn:
+    BATCH_SIZE = 5000
+
     # Abrir un cursor para poder realizar operaciones de database
     cur = conn.cursor()
 
@@ -21,26 +32,26 @@ with psycopg2.connect(dbname="recomendaciones_steam", user="postgres", password=
     print("Insertando datos en la tabla games")
     with open ('preprocesado/output_steam_games.csv') as f:
         data = list(csv.reader(f))[1:]
-        for n in tqdm(range(0, len(data), 5000)):
-            extras.execute_values(cur, "INSERT INTO games (item_id, item_name, year_release) VALUES %s;", data[n : n + 5000])
+        for n in tqdm(range(0, len(data), BATCH_SIZE)):
+            extras.execute_values(cur, "INSERT INTO games (item_id, item_name, year_release) VALUES %s;", data[n : n + BATCH_SIZE])
 
     print("Insertando datos en la tabla game_genres")
     with open ('preprocesado/output_steam_game_genres.csv') as f:
         data = list(csv.reader(f))[1:]
-        for n in tqdm(range(0, len(data), 5000)):
-            extras.execute_values(cur, "INSERT INTO game_genres (item_id, genre) VALUES %s;", data[n : n + 5000])
+        for n in tqdm(range(0, len(data), BATCH_SIZE)):
+            extras.execute_values(cur, "INSERT INTO game_genres (item_id, genre) VALUES %s;", data[n : n + BATCH_SIZE])
     
     print("Insertando datos en la tabla playtime")    
     with open ('preprocesado/australian_users_items.csv') as f:
         data = list(csv.reader(f))[1:]
-        for n in tqdm(range(0, len(data), 5000)):
-            extras.execute_values(cur, "INSERT INTO playtime (user_id, item_id, playtime) VALUES %s;", data[n : n + 5000])
+        for n in tqdm(range(0, len(data), BATCH_SIZE)):
+            extras.execute_values(cur, "INSERT INTO playtime (user_id, item_id, playtime) VALUES %s;", data[n : n + BATCH_SIZE])
     
     print("Insertando datos en la tabla reviews")
     with open ('preprocesado/australian_user_reviews.csv') as f:
         data = list(csv.reader(f))[1:]
-        for n in tqdm(range(0, len(data), 5000)):
-            extras.execute_values(cur, "INSERT INTO reviews (user_id, item_id, recommend, year_review, sentiment) VALUES %s;", data[n : n + 5000])
+        for n in tqdm(range(0, len(data), BATCH_SIZE)):
+            extras.execute_values(cur, "INSERT INTO reviews (user_id, item_id, recommend, year_review, sentiment) VALUES %s;", data[n : n + BATCH_SIZE])
 
     print("Insersión completa")
     conn.commit()
